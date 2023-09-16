d3f94cad04f8af86a9fc7f947539249e17225f6d
slowrunr <mungaslov.nft@gmail.com>
2023-09-16 23:59:49 +0900

    connected to omdb api

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function getActionFromApi() {
-  fetch("http://www.boredapi.com/api/activity")
+// const DEFAULT_INTRO_TEXT = "🤔Стало скучно?";
+// const DEFAULT_MAIN_TEXT = "Не знаешь, чем заняться?";
+// const ACTIVE_INTRO_TEXT = "УРА! Теперь не скучно!";
+
+// const introTextNode = document.getElementById("introText");
+// const actionOutputNode = document.getElementById("actionOutput");
+const searchBtnNode = document.getElementById("searchBtn");
+
+function getMovieFromApi() {
+  fetch("http://www.omdbapi.com/?i=tt3896198&apikey=5b10ef8b")
     .then((response) => response.json())
     .then((res) => {
       if (res.ok) {
         return res;
       }
-      const actionFromApi = res.activity;
+      const resFromApi = res.activity;
       //   console.log(actionFromApi);
-      actionOutputNode.innerText = `${actionFromApi}`;
+      console.log(resFromApi);
     });
 }
+
+function searchMovieBtnHandler() {
+  getMovieFromApi();
+}
+
+searchBtnNode.addEventListener("click", searchMovieBtnHandler);
