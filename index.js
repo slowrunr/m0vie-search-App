121762dd70c7a61896f99ad53db524e61a55255a
slowrunr <mungaslov.nft@gmail.com>
2023-09-19 23:59:30 +0900

    fixed render api response. n2b tuned

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const API_URL = "https://www.omdbapi.com/?s=batman&apikey=5b10ef8b";
 const API_PERSONAL_KEY = "5b10ef8b";
 const BODY_FIXED = "body-fixed";
+const POSITIVE_RESPONSE = "True";
 const ERROR_MESSAGE_TEXT = "No such movie found 🤔";
 const STATUS_OUT_OF_DATA_CLASSNAME = "border-red";
 
 const titleInputNode = document.getElementById("titleInput");
 const searchBtnNode = document.getElementById("searchBtn");
-const movieSearchResultsNode = document.getElementById("movieSearchResults");
+const movieListNode = document.getElementById("movieList");
 
 let movieList = [];
 
@@ -14,36 +15,41 @@ function getMovieFromOMDB() {
   const movieTitle = titleInputNode.value;
   fetch(`http://www.omdbapi.com/?apikey=${API_PERSONAL_KEY}&s=${movieTitle}`)
     .then((response) => response.json())
-    .then((searchResults) => {
-      console.log(searchResults);
-      movieList = searchResults.Search;
-    });
-  //need to be fixed!!!!!
-  if (response === "True") {
-    searchResults.Search.forEach((movie) => {
-      const moviePoster = movie.Poster;
-      const movieTitle = movie.Title;
-      const movieYear = movie.Year;
-      const movieType = movie.Type;
-      //const fallbackImage = "resourses/undefined-movie.png";
-
-      const movieHTML = `
-        <li class="js-movie movie">
-          <div class="col">
-            <img class="movie-img" src="${moviePoster}" alt="${movieTitle}" onerror="this.src='${fallbackImage}'">
-          </div>
-          <div class="col">
-            <h2 class="movie-title">${movieTitle}</h2>
-            <p class="movie-year">${movieYear}</p>
-            <p class="movie-type">${movieType}</p>
-          </div>
-        </li>
-      `;
-
-      movieSearchResultsNode.insertAdjacentHTML("beforeend", movieHTML);
+    .then((res) => {
+      if (res.ok) {
+        return res;
+      }
+      const movieSearchResult = res.Search;
+      console.log(movieSearchResult);
+      movieListNode.innerText = `${movieSearchResult}`;
+      //renderMovieList();
     });
 
-    clearInput();
+  clearInput();
+}
+
+function renderMovieList() {
+  movieListNode.innerHTML = "";
+
+  if (response === POSITIVE_RESPONSE) {
+    results.Search.forEach((movie) => {
+      const renderedMovie = document.createElement("li");
+      renderedMovie.setAttribute("id", movie.omdbID);
+      renderedMovie.classList.add("movie-wrapper");
+
+      // renderedMovie.innerHTML = `
+      //   <div class="movies__img-container">
+      //     <img class="movies__img" src="${movie.Poster}" alt="${movie.Title}">
+      //   </div>
+      //     <div class="movies__content-container">
+      //     <h2 class="movies__title">Name: <span>${movie.Title}</span></h2>
+      //     <p class="movies__year">Year: <span>${movie.Year}</span></p>
+      //     <p class="movies__type">Type: <span>${movie.Type}</span></p>
+      //   </div>
+      // `;
+
+      movieListNode.insertAdjacentHTML("beforeend", renderedMovie);
+    });
   }
 }
 
