c8b3f70e9d656c93b7bdcc673ea70ee41773123a
slowrunr <mungaslov.nft@gmail.com>
2023-10-02 00:47:43 +0900

    movie card ready

--- scripts/movie.js
+++ scripts/movie.js
@@ -31,22 +31,21 @@ function getMovieInfoFromApiAndRender() {
       const backupImage = NO_POSTER_IMAGE;
       movieCardNode.innerHTML = `
         <h2 class="movie__title">${Title}</h2>
-        <div class="movie__card">
+        <div class="movie__card-inner">
             <div class="poster__wrapper">
             <img class="movie-poster" src="${Poster}" alt="${Title}" onerror="this.src='${backupImage}'">
             </div>
             <div class="movie__info">
+                <p class="descriptor"> Director: <span class="higlighted">${Director}</span></p>
                 <p class="descriptor">Year: <span class="higlighted">${Year}</span></p>
+                <p class="descriptor"> Actors: <span class="higlighted">${Actors}</span></p>
                 <p class="descriptor"> Rating: <span class="higlighted">${Rated}</span></p>
                 <p class="descriptor"> Awards: <span class="higlighted">${Awards}</span></p>
-                <p class="descriptor"> Runtime: <span class="higlighted">${Runtime}</span></p>
                 <p class="descriptor"> Genre: <span class="higlighted">${Genre}</span></p>
-                <p class="descriptor"> Director: <span class="higlighted">${Director}</span></p>
-                <p class="descriptor"> Actors: <span class="higlighted">${Actors}</span></p>
-                <p class="descriptor"> Plot: <span class="higlighted">${Plot}</span></p>
+                <p class="descriptor"> Runtime: <span class="higlighted">${Runtime}</span></p>
             </div>  
         </div>
-        
+        <p class="descriptor movie__plot">... ${Plot} </p>
         
      `;
 
